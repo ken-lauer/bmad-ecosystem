09b7817a7639b3d0dbe009e005becaff7efae515
Ken Lauer <152229072+ken-lauer@users.noreply.github.com>
2024-12-11 14:45:36 -0800

    CLN: clean up some testing

--- tao/fpm-noplot/app/main.f90
+++ tao/fpm-noplot/app/main.f90
@@ -1,17 +1,1 @@
-!+
-! Program tao_program
-!
-! Program wrapper
-!-
-
-program tao_program
-
-use tao_interface
-
-implicit none
-
-! Call tao_top_level and we are done.
-
-call tao_top_level()
-
-end program
+../../program/tao_program.f90
